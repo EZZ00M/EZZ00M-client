858389186524424558399ea7672914194f8eab05
Jeonghyun <limithyunii@gmail.com>
2024-11-19 13:31:02 +0900

    Feat: 헤더 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import * as S from "./Header.style";
+import Logo from "../../assets/logo.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <S.Container>
+      <div>
+        <S.Logo
+          src={Logo}
+          alt='logo'
+        />
+      </div>
+      <S.MenuBox>
+        <S.Menu>Menu 1</S.Menu>
+        <S.Menu>Menu 2</S.Menu>
+      </S.MenuBox>
+    </S.Container>
+  );
 };
 
 export default Header;
---
+++ src/components/Header/Header.style.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  min-width: 300px;
+  height: 70px;
+  padding: 10px 20px;
+  border-bottom: 1px solid #d3d3d3;
+  justify-content: space-between;
+`;
+
+export const Logo = styled.img`
+  display: flex;
+  width: 50px;
+  height: 50px;
+`;
+
+export const MenuBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 200px;
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  font-size: 16px;
+`;
