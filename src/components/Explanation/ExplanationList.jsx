8e7e32c8298348a275d14434cfd640e00703d505
Jeonghyun <limithyunii@gmail.com>
2024-11-30 13:38:10 +0900

    Feat: 메인페이지 하단 설명 컴포넌트 추가

---
+++ src/components/Explanation/ExplanationList.jsx
@@ -0,0 +1,28 @@
+import {
+  MdOutlineLightbulb,
+  MdAlarm,
+  MdOutlineRemoveRedEye,
+} from "react-icons/md";
+
+const ExplanationList = [
+  {
+    icon: <MdOutlineLightbulb />,
+    title: "어떻게 사용하나요?",
+    content:
+      "파일을 상자 안으로 드래그 앤 드롭하거나 파일 선택 버튼을 클릭해서 업로드할 파일을 검색합니다. 파일은 즉시 업로드되며, 오직 .csv, .xlsx, .xls 형식의 파일만 업로드할 수 있습니다. 변환이 완료되면 파일을 다운로드할 수 있습니다.",
+  },
+  {
+    icon: <MdAlarm />,
+    title: "무엇을 위해 사용하나요?",
+    content:
+      "이 프로그램은 화상 통화 프로그램 줌의 결과를 추출하기 위한 프로그램입니다. 미팅의 주최자가 미팅에 누가 얼마나 오랫동안 참여하였는지 판단할 수 있도록 하여 쉽게 참여자들의 머무른 시간을 파악할 수 있습니다.",
+  },
+  {
+    icon: <MdOutlineRemoveRedEye />,
+    title: "왜 사용하나요?",
+    content:
+      "줌에서 제공하는 기본적인 로그는 미팅 도중 참여자가 나갔다가 들어오는 경우 별개의 사용자로 인식하여 로그가 여러 번 찍히고, 일정 시간 이상 머무른 참가자를 구분해내기 위해 직접 눈으로 파악해야하는 문제가 있습니다.",
+  },
+];
+
+export default ExplanationList;
