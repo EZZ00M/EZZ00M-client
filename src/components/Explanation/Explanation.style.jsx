3dbcf4b7b218e87f676833ac84d74f9eda088d18
Jeonghyun <limithyunii@gmail.com>
2025-01-12 23:54:52 +0900

    fix: 설명 문구 변경

--- src/components/Explanation/Explanation.style.jsx
+++ src/components/Explanation/Explanation.style.jsx
@@ -14,7 +14,6 @@ export const Wrapper = styled.div`
 
 export const Container = styled.div`
   display: flex;
-  flex: 1;
   flex-direction: column;
   justify-content: center;
   align-items: center;
