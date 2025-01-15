d2730d303352e5968259f01728c78a60f8c39cda
Jeonghyun <limithyunii@gmail.com>
2025-01-15 22:26:03 +0900

    fix: 사이트 설명 문장 변경

--- src/components/DragBox/StateComponent/FileSelectedState.jsx
+++ src/components/DragBox/StateComponent/FileSelectedState.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 import * as S from "../DragBox.style";
 
-const FileSelectedState = ({ files, onAnalyze }) => (
-  <>
-    <S.InnerBox>
-      <S.StyledFilesContainer>
-        {files.map((file) => (
-          <S.StyledFileName key={file.name}>
-            <S.FileIcon />
-            {file.name}
+const FileSelectedState = ({ files, onAnalyze, onRemove, onAddFiles }) => (
+  <S.InnerBox>
+    <S.StyledFilesContainer>
+      {files.map((file) => (
+        <S.StyledFlexBox key={file.name}>
+          <S.StyledFileName>
+            <span>{file.name}</span>
+            <div>
+              <S.FileIcon
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onRemove(file.name);
+                }}
+              />
+            </div>
           </S.StyledFileName>
-        ))}
-      </S.StyledFilesContainer>
+        </S.StyledFlexBox>
+      ))}
+    </S.StyledFilesContainer>
+
+    <S.ButtonContainer>
       <S.Button onClick={onAnalyze}>로그 분석하기</S.Button>
-    </S.InnerBox>
-  </>
+      <S.Button
+        onClick={(e) => {
+          e.stopPropagation();
+          onAddFiles();
+        }}
+      >
+        파일 추가하기
+      </S.Button>
+    </S.ButtonContainer>
+  </S.InnerBox>
 );
 
 export default FileSelectedState;
