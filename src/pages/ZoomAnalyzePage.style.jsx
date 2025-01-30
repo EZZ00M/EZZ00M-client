3ad8787798c87a58e8d39837b0d2d38bba87d795
misung.dev <misung.dev@gmail.com>
2025-01-30 19:05:40 +0900

    feat: 페이지 타이틀 반응형 적용

--- src/pages/ZoomAnalyzePage.style.jsx
+++ src/pages/ZoomAnalyzePage.style.jsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import { BREAKPOINTS } from "../styles/mediaQueries";
+
+export const ZoomAnalyzePage = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Container = styled.div`
   display: flex;
@@ -9,15 +15,29 @@ export const Container = styled.div`
   padding-top: 100px;
 `;
 
-export const Text = styled.p`
+export const Title = styled.h1`
   text-align: center;
   font-size: 40px;
   font-weight: 800;
+
+  @media (max-width: ${BREAKPOINTS[1]}px) {
+    font-size: 36px;
+    font-weight: 700;
+  }
+  @media (max-width: ${BREAKPOINTS[0]}px) {
+    font-size: 32px;
+  }
 `;
 
-export const SmallText = styled.p`
+export const SubTitle = styled.p`
   text-align: center;
   padding: 10px;
-  font-size: 25px;
-  font-weight: 400;
+  font-size: 26px;
+
+  @media (max-width: ${BREAKPOINTS[1]}px) {
+    font-size: 22px;
+  }
+  @media (max-width: ${BREAKPOINTS[0]}px) {
+    font-size: 18px;
+  }
 `;
